f50e4c5df0420ebf380a0d32ef5e25b3261e2a91
caitlinburns <ceburns1@loyola.edu>
2024-10-22 16:43:48 -0400

    updated code and RD2

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from logging import DEBUG
+# Programmers:  Caitlin Burns and Korede Oni
+# Course:  CS151, Professor Zee
+# Due Date: 10/23/2024
+# Lab Assignment: 6
+# Problem Statement: This code allows a user to select from a menu of ATM options using functions
+# Data In: deposit, withdrawal, view, exit, and amounts to deposit/withdraw
+# Data Out: total balance or negative balance warning
+# Credits: building on code from lab 5
 
-
+#this runs the main menu of the ATM
 def input_choice():
     choice = input("\nPlease select an option:"
           "\n\t D - Deposit"
@@ -13,21 +20,28 @@ def input_choice():
         choice = input("\nPlease select an option:")
     return choice
 
-
+#this checks for negative numbers and reroutes the user
 def error_return(int_check):
     if int_check < 0:
         print('Input a valid number')
 
-
+#this allows the user to make a deposit
 def deposit(balance):
     deposit_amt = int(input('How much would you like to deposit?: '))
+    while deposit_amt < 0:
+        error_return(deposit_amt)
+        deposit_amt = int(input('How much would you like to deposit?: '))
     error_return(deposit_amt)
     balance += deposit_amt
     print(f'Your balance is ${balance:.2f}')
     return balance
 
+#this allows the user to make a withdrawal
 def withdraw(balance):
     withdraw_amt = int(input('How much would you like to withdraw?: '))
+    while withdraw_amt < 0:
+        error_return(withdraw_amt)
+        withdraw_amt = int(input('How much would you like to withdraw?: '))
     error_return(withdraw_amt)
     balance -= withdraw_amt
     print(f'Your balance is ${balance:.2f}')
@@ -35,12 +49,15 @@ def withdraw(balance):
         print('You will be charged 5% interest.')
     return balance
 
+#this allows the user to view the balance
 def view_balance(balance):
     print(f'Your balance is ${balance}')
 
+#this allows the user to exit the loop
 def exit():
     print('Thank you for using the ATM')
 
+#this runs the main part of the program
 def main():
     print('This code allows a user to select from a list of options at an ATM. \nThey can deposit, withdraw, or check balance')
     balance = 1000
